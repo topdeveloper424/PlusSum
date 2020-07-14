13a14b930342121fb694867c7305c7af73cd34d1
top developer <mingming424224@gmail.com>
2020-07-14 09:00:16 -0400

    added web config

--- PlusSum/settings.py
+++ PlusSum/settings.py
@@ -144,5 +144,5 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
