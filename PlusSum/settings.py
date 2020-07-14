a76bd272fc424cc2ef523c2b007531de897a0620
top developer <mingming424224@gmail.com>
2020-07-14 04:32:11 -0400

    changed static path

--- PlusSum/settings.py
+++ PlusSum/settings.py
@@ -144,5 +144,5 @@ STATICFILES_DIRS = [
 
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
-MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
